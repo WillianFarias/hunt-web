bae275f656d9ca96f8dea9f7d08f9abc9c9710b3
WillianFarias <williansousafarias@gmail.com>
2020-10-21 19:59:02 -0300

    criando header, aplicando estilizacao com css e criando componente header

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import "./styles.css";
+
+//Constante que possui uma funcao com retorno
+const Header = () => (
+  <header id="main-header">JSHunt</header>
+);
+
+export default Header;
+
+/*
+class Header extends Component {
+  render() {
+    return <h1>Hello</h1>
+  }
+}*/
