45e6e893dc2a675b59144e5be9184ee933e4d782
WillianFarias <williansousafarias@gmail.com>
2020-10-22 10:47:40 -0300

    personalizando pag de listagem de produtos

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import api from '../../services/api';
 
+import "./styles.css";
+
 export default class Main extends Component {
 
   //state é sempre um array
@@ -23,14 +25,22 @@ export default class Main extends Component {
 
   render() {
   //return <h1>Contagem de produtos: {this.state.products.length}</h1>
-  return (
-    <div className="product-list">
-      {
-        this.state.products.map(product => (
-        <h2 key={product._id}>{product.title}</h2>
-        ))
-      }
-    </div>
-  );
+    const { products } = this.state;
+
+    return (
+      <div className="product-list">
+        {
+          products.map(product => (
+          //<h2 key={product._id}>{product.title}</h2>
+          <article key={product._id}>
+            <strong>{product.title}</strong>
+          <p>{product.description}</p>
+
+          <a href="">Acessar</a>
+          </article>
+          ))
+        }
+      </div>
+    );
   }
 }
