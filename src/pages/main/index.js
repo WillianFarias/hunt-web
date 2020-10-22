a9bbd0df36aad6169feb6470c88c01942130271f
WillianFarias <williansousafarias@gmail.com>
2020-10-22 10:31:25 -0300

    listando produtos na pag, armazenando no estado

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 import api from '../../services/api';
 
 export default class Main extends Component {
+
+  //state é sempre um array
+  state = {
+    products: [],
+  }
+
   //Componente executado assim que o componente é renderizado em tela
   componentDidMount() {
     this.loadProducts();
@@ -11,10 +17,20 @@ export default class Main extends Component {
   loadProducts = async () => {
     const response = await api.get('/products');
 
-    console.log(response.data.docs);
+    //console.log(response.data.docs);
+    this.setState({ products: response.data.docs });
   }
 
   render() {
-    return <h1>Hello Rocketseat</h1>
+  //return <h1>Contagem de produtos: {this.state.products.length}</h1>
+  return (
+    <div className="product-list">
+      {
+        this.state.products.map(product => (
+        <h2 key={product._id}>{product.title}</h2>
+        ))
+      }
+    </div>
+  );
   }
 }
